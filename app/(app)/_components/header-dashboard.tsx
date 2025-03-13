bd6a11ec35ae4f6eab5e0c87d1507cbd31a90c9d
Matheus Serafim <matheus18serafim@gmail.com>
2025-03-13 20:47:59 -0300

    feat: add HeaderDashboard component with dialog for AI report and date selection

---
+++ app/(app)/_components/header-dashboard.tsx
@@ -0,0 +1,30 @@
+import DialogReport from "@/app/_components/dialog-report";
+import { Button } from "@/app/_components/ui/button";
+import { Dialog, DialogTrigger } from "@/app/_components/ui/dialog";
+import { ClipboardMinusIcon } from "lucide-react";
+import TimeSelected from "./date-selected";
+
+const HeaderDashboard = () => {
+  return (
+    <div className="mb-5 flex items-center justify-between">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+
+      <div className="flex items-center gap-4">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">
+              Relatório com IA
+              <ClipboardMinusIcon className="ml-1" />
+            </Button>
+          </DialogTrigger>
+
+          <DialogReport />
+        </Dialog>
+
+        <TimeSelected />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderDashboard;
